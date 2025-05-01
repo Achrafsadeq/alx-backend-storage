428c42fc42f497f45ff6f2ce459e02d75a86e474
Achrafsadeq <achrafsadeqq@gmail.com>
2025-05-01 20:55:54 +0000

    Compute average weighted score for user

---
+++ 100-average_weighted_score.sql
@@ -0,0 +1,19 @@
+-- Procedure to compute the average weighted score for a student
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE weighted_avg_score FLOAT;
+    
+    SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+    INTO weighted_avg_score
+    FROM corrections
+    INNER JOIN projects ON corrections.project_id = projects.id
+    WHERE corrections.user_id = user_id;
+    
+    UPDATE users
+    SET average_score = weighted_avg_score
+    WHERE id = user_id;
+END$$
+DELIMITER ;
