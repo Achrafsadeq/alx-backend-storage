79aa7c63fec84a6fc8132e1156330090db8f79a3
Achrafsadeq <achrafsadeqq@gmail.com>
2025-05-01 20:55:53 +0000

    Create a table with country attribute

---
+++ 1-country_users.sql
@@ -0,0 +1,9 @@
+-- 1. In and not out
+-- This script creates a 'users' table with a country ENUM constraint.
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
