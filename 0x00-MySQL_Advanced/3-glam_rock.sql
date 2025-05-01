385fa2cc8362cc93e5658b14265570b62e60e5b4
Achrafsadeq <achrafsadeqq@gmail.com>
2025-05-01 21:24:48 +0000

    List bands with Glam rock as main style

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,14 +1,6 @@
 -- 3. Old school band
 -- Lists Glam rock bands ranked by longevity (until 2022)
-
-SELECT name AS band_name,
-       IFNULL(
-           (2022 - formed),
-           0
-       ) - IFNULL(
-           (2022 - split),
-           0
-       ) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
