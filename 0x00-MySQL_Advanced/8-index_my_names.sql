465af37781ed620f9673b57f2cd55c238a1f51b2
Achrafsadeq <achrafsadeqq@gmail.com>
2025-05-01 21:24:48 +0000

    Create index on first letter of name

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- 8. Optimize simple search
 -- This script creates an index idx_name_first on the first letter
 -- of the 'name' column in the 'names' table.
-CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));
+CREATE INDEX idx_name_first ON names(name(1));
