90d658f778a7d96f829797873dfb76f1345086fe
Achrafsadeq <achrafsadeqq@gmail.com>
2025-05-06 17:03:52 -0700

    Python function to list all documents in collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module to list all documents in a collection."""
+
+
+def list_all(mongo_collection):
+    """Lists all documents in a MongoDB collection."""
+    return list(mongo_collection.find()) if mongo_collection else []
